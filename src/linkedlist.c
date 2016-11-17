def0c30fb3c3660558428b054131d7d72b7fde1c
Sandesh Ghimire <sandeshghimire@gmail.com>
2016-11-16 21:58:15 -0800

    Added Functions prototype

---
+++ src/linkedlist.c
@@ -0,0 +1,48 @@
+#include "linkedlist.h"
+
+int init_linkedlist(struct linkedlist **head)
+{
+    return 0;
+}
+int length(struct linkedlist **head)
+{
+    return 0;
+}
+int print(struct linkedlist **head)
+{
+    return 0;
+}
+int reverse(struct linkedlist **head)
+{
+    return 0;
+}
+int sort(struct linkedlist **head)
+{
+    return 0;
+}
+
+int enqueue(struct linkedlist **head, int item)
+{
+    return 0;
+}
+int dequeue(struct linkedlist **head)
+{
+    return 0;
+}
+int push(struct linkedlist **head, int item)
+{
+    return 0;
+}
+int pop(struct linkedlist **head)
+{
+    return 0;
+}
+
+int add_to_list(struct linkedlist **head, int item, int index)
+{
+    return 0;
+}
+int remove_from_list(struct linkedlist **head, int index)
+{
+    return 0;
+}
--- src/linkedlist.h
+++ src/linkedlist.h
@@ -2,6 +2,33 @@
 #define LINKEDLIST_H
 
 
+#include <stdio.h>
+#include <stdlib.h>
+
+struct linkedlist
+{
+    int data;
+    struct linkedlist * next;
+    struct linkedlist * prev;
+}linkedlist;
+
+
+int init_linkedlist(struct linkedlist ** head);
+int length(struct linkedlist ** head);
+int print(struct linkedlist ** head);
+int reverse(struct linkedlist ** head);
+int sort(struct linkedlist ** head);
+
+
+int enqueue(struct linkedlist ** head, int item);
+int dequeue(struct linkedlist ** head);
+int push(struct linkedlist ** head, int item);
+int pop(struct linkedlist ** head);
+
+
+int add_to_list(struct linkedlist ** head, int item, int index);
+int remove_from_list(struct linkedlist ** head,  int index);
+
 
 
 #endif /*LINKEDLIST_H*/
